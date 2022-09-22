d25afc43c7c88fa3c47c125c0de5e5c68e4676bb
Md. Nazrul Chowdhury <nazrul.chowdhury@glorifi.com>
2022-09-22 19:25:26 +0600

    responsive header added

---
+++ lib/app_style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ci_cd_practice/constants.dart';
+
+class AppStyle {
+  static const logoTextStyle = TextStyle(
+    fontSize: logoFontSize,
+    fontWeight: FontWeight.bold,
+    color: logoColor,
+  );
+
+  static const itemTextStyle = TextStyle(
+    fontSize: itemFontSize,
+    color: itemColor,
+  );
+}
---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const logoFontSize = 24.0;
+const itemFontSize = 20.0;
+
+const logoColor = Colors.black;
+const itemColor = Colors.blue;
